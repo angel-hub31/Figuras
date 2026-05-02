ca30d6d8ce3c3f59f1587b546a93a77da6005a74
angel-hub31 <moralesmales1@hotmail.com>
2026-05-01 22:06:49 -0500

    Se crea constructores en la clase Figuras

--- src/com/krakedev/figuras/Figuras.java
+++ src/com/krakedev/figuras/Figuras.java
@@ -1,32 +1,43 @@
 package com.krakedev.figuras;
 
 public class Figuras {
-	//atributos privados 
+	// atributos privados
 	private String nombre;
 	private String color;
+
+//constructor con parametros
+	public Figuras(String nombre, String color) {
+
+		this.nombre = nombre;
+		this.color = color;
+	}
+
+//constructor sin parametros o vacio
+	public Figuras() {
+
+	}
 
 // creacion de metodos getters y setters
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getColor() {
 		return color;
 	}
+
 	public void setColor(String color) {
 		this.color = color;
 	}
-	
+
 	// sobreescritura del metodo toString
 	@Override
 	public String toString() {
-		return "Figuras: nombre=" + nombre + ", color=" + color ;
+		return "Figuras: nombre=" + nombre + ", color=" + color;
 	}
-	
-	
-	
-	
 
 }
