05356602eea4ff42e3665d7fa3a416e16ded65dd
angel-hub31 <moralesmales1@hotmail.com>
2026-05-01 21:45:33 -0500

    Se crea la clase Figura con sus atributos privados, se crea metodos getters y setters y se sobreescribe el metodo toString

--- src/com/krakedev/figuras/Figuras.java
+++ src/com/krakedev/figuras/Figuras.java
@@ -1,5 +1,32 @@
 package com.krakedev.figuras;
 
 public class Figuras {
+	//atributos privados 
+	private String nombre;
+	private String color;
+
+// creacion de metodos getters y setters
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	// sobreescritura del metodo toString
+	@Override
+	public String toString() {
+		return "Figuras [nombre=" + nombre + ", color=" + color + "]";
+	}
+	
+	
+	
+	
 
 }
